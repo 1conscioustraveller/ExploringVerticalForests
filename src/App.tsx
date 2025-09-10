36f644ca480ca4ea77addd7c9eb30acb70a67430
1conscioustraveller <1conscioustraveller@gmail.com>
2025-09-10 13:51:23 -0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ParticleTextEffect } from "./components/ui/particle-text-effect";
+
+function App() {
+  return (
+    <div>
+      <ParticleTextEffect />
+    </div>
+  );
+}
+
+export default App;
